730d54cb93c43d21d558935a9e8542f41284d0e7
swamyhr <swamyrangannanayaka@gmail.com>
2021-11-28 16:33:31 +0530

    folderName changed to ImageSearch and Code modified

--- my-app/pages/imageSearch/[query].tsx
+++ my-app/pages/imageSearch/[query].tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import Link from 'next/link'
+import Link from 'next/link';
 
 var a: number = 10; 
 // public search_image_text: string = "";
@@ -15,20 +15,20 @@ function ImageSearch({ unspalsh_images }: any){
     async function fetchImages(image_search_text: string)
     {
         // Creating Request URL with query parameter and client_id
-        let unspalsh_api_url = `https://api.unsplash.com/search/photos?query=${image_search_text}&client_id=${process.env.NEXT_PUBLIC_CLIET_ID_FOR_UNSPLASH_API_KEY}`;
-        // axios.get(`http://localhost:3000/imageSearch/${image_search_text}`).then(res=>{console.log("res")}).catch((error)=>console.log(error));
+        // let unspalsh_api_url = `https://api.unsplash.com/search/photos?query=${image_search_text}&client_id=${process.env.NEXT_PUBLIC_CLIET_ID_FOR_UNSPLASH_API_KEY}`;
+        axios.get(`http://localhost:3000/imageSearch/${image_search_text}`).then(res=>{console.log("res")}).catch((error)=>console.log(error));
 
         // Calling UnsplashAPI with axios package  
-        await axios.get(unspalsh_api_url).then((res) => {
-            if(res.data.results)
-            {
-                // getServerSideProps(image_search_text);
-                // setUnsplashImages(unspalsh_images);
-                setUnsplashImages(res.data.results);
-            }
-        }).then((error)=>{ 
-            console.log(error);
-        });
+        // await axios.get(unspalsh_api_url).then((res) => {
+            // if(res.data.results)
+            // {
+              // getServerSideProps(image_search_text);
+                setUnsplashImages(unspalsh_images);
+                // setUnsplashImages(res.data.results);               
+            // }
+        // }).then((error)=>{ 
+            // console.log(error);
+        // });
     }
 
     // Call API only if user Input has some value
@@ -36,17 +36,21 @@ function ImageSearch({ unspalsh_images }: any){
     {
         if(searchText.length > 0)
         {
+            console.log(searchText);
             await fetchImages(searchText);
+            // search_image_text = searchText;
         }
         else
         {
-            // If search text is empty
             alert("Please Enter Some Text..")
         }
     }
 
     return(
         <>
+            <Link href="/imageSearch">
+                <a>Page Search</a>
+            </Link>
             <div className="head-container">
                 <h1>Image Search Application with Unspalsh API</h1>
                     <input className="input-search" type="text" name="image_search_text" 
@@ -66,7 +70,9 @@ function ImageSearch({ unspalsh_images }: any){
 export default ImageSearch;
 
 export async function getServerSideProps(context: any) {
-    const image_response = await fetch(`https://api.unsplash.com/search/photos?query=Java&client_id=${process.env.NEXT_PUBLIC_CLIET_ID_FOR_UNSPLASH_API_KEY}`);
+    const {params} = context;
+    const {query} = params;
+    const image_response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.NEXT_PUBLIC_CLIET_ID_FOR_UNSPLASH_API_KEY}`);
     const images = await image_response.json();
 
     return {
